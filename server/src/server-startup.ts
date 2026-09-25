91de64752a60837a0457823c2c6eec3dc6d17cf5
Czvein Lei Unarce <czveinlei17@gmail.com>
2026-09-26 04:14:04 +0800

    chore: remove server startup tests and implementation

--- server/src/server-startup.ts
+++
@@ -1,7 +0,0 @@
-export async function startServer(
-	checkDatabaseConnection: () => Promise<void>,
-	startListening: () => void,
-): Promise<void> {
-	await checkDatabaseConnection();
-	startListening();
-}
